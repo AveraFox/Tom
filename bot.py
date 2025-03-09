ff0a0f540165fea5c909104f2d05ec6087ba7fa0
Nocrex <20463817+Nocrex@users.noreply.github.com>
2025-03-09 14:20:11 +0100

    Add event error handling

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
-import discord, logging, os
+import discord, logging, os, statics, sys
 import discord.ext.commands
+from io import StringIO
+import traceback
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -9,12 +11,24 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(level=logging.INFO, format="{levelname:8s} | {asctime} | {name:15s} | {message}", style="{")
 
+error_channel = None
+
 @bot.event
 async def on_ready():
+    global error_channel
+    error_channel = await bot.fetch_channel(statics.ERROR_CHANNEL_ID)
     await bot.load_extension("hp_cog") # load hp_cog.py into the bot
     await bot.load_extension("vanity_resolver_cog")
     await bot.tree.sync() # upload command tree to discord, so you can see all available commands in the client
     logger.info(f"{bot.user} is up and running meow")
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    error = sys.exception()
+    msg = "".join(traceback.format_exception(error))
+    sio = StringIO(msg)
+    await error_channel.send(file=discord.File(sio, filename="error.txt"))
+    logger.error(msg)
 
 if os.environ.get("DEBUG") == "1": # only enable command if debug is set in environment
     @bot.tree.command()
