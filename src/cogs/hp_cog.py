3bc1483571ccda257f701e11cea878c1b8e65e9e
Nocrex <20463817+Nocrex@users.noreply.github.com>
2026-05-09 00:15:16 +0200

    fix python nagging

--- src/cogs/hp_cog.py
+++ src/cogs/hp_cog.py
@@ -387,7 +387,7 @@ class HPCog(commands.Cog):
         not_reported_ids = set(steamids_list) - set(report.players.keys())
         
         if len(not_reported_ids) > 0:
-            await interaction.response.send_message(f"**These players were not in this report**\n{'\n'.join([str(id) for id in not_reported_ids])}", ephemeral=True)
+            await interaction.response.send_message(f"**These players were not in this report**\n{chr(10).join([str(id) for id in not_reported_ids])}", ephemeral=True)
             return
             
         if date is None:
